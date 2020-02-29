44d476403ddd4a29a9a2dcb0b47324b1dc63cac9
cc <siukchung@hotmail.co.uk>
2020-02-29 19:22:14 +0000

    Removed debug events in verifier

--- contracts/verifiers/TradeInitiatedEventVerifier.sol
+++ contracts/verifiers/TradeInitiatedEventVerifier.sol
@@ -5,13 +5,7 @@ import "../libraries/RLP.sol";
 
 contract TradeInitiatedEventVerifier is EventVerifier {
     bytes32 eventSignature = keccak256("TradeInitiated(bytes32)");
-
-    event TradeInitiated(bytes32 hash);
-    event InitiatorCancelled(bytes32 hash);
-    event TradeResponded(bytes32 hash, bytes32 hash2);
-    event ResponderCancelled(bytes32 hash, bytes32 hash2);
-    event Confirmed(bytes32 hash, bytes32 hash2);
-
+    
     function verify(bytes20 _contractEmittedAddress, bytes memory _rlpReceipt, bytes32 _expectedCommitment) public view returns (bool) {
         // Retrieve specific log for given event signature
         RLP.RLPItem[] memory logs = retrieveLogs(eventSignature, _contractEmittedAddress, _rlpReceipt);
@@ -27,13 +21,5 @@ contract TradeInitiatedEventVerifier is EventVerifier {
             }
         }
         return false;
-    }
-
-    function doEvent() public {
-        emit InitiatorCancelled(0x3471555ab9a99528f02f9cdd8f0017fe2f56e01116acc4fe7f78aee900442f35);
-    }
-
-    function doEvent2() public {
-        emit Confirmed(0x3471555ab9a99528f02f9cdd8f0017fe2f56e01116acc4fe7f78aee900442f35, 0x07f36c7ad26564fa65daebda75a23dfa95d660199092510743f6c8527dd72586);
     }
 }
